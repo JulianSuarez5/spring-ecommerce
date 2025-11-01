70e1afc46fa20e414ea96c8bedd783efa4fc9466
JulianSuarez5 <Juacosuarez12@gmail.com>
2025-11-01 08:48:01 -0500

    Nevos cambios realizados, arreglo de la base de datos rota y arreglar algunos comentarios

--- src/main/java/ppi/e_commerce/Controller/HomeController.java
+++ src/main/java/ppi/e_commerce/Controller/HomeController.java
@@ -1,26 +1,11 @@
 package ppi.e_commerce.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ppi.e_commerce.Service.*;
-import ppi.e_commerce.Model.Product;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private ProductService productService;
-
-    @Autowired
-    private CategoryService categoryService;
-
-    @Autowired
-    private BrandService brandService;
 
     @GetMapping("/")
     public String home() {
